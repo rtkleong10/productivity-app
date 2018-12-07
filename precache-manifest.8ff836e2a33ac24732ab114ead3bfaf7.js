6fcf8ae9e23a832a9e575e3a56e9f48d8c7a24ab
rtkleong10 <rtkleong10@gmail.com>
2018-12-07 21:30:47 +0800

    Updates

---
+++ precache-manifest.8ff836e2a33ac24732ab114ead3bfaf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1d4de3166916ef4b5ac",
+    "url": "/productivity-app/static/js/runtime~main.e1d4de31.js"
+  },
+  {
+    "revision": "5b39ef2b605ad071cb88",
+    "url": "/productivity-app/static/js/main.5b39ef2b.chunk.js"
+  },
+  {
+    "revision": "cd822512173c83760be0",
+    "url": "/productivity-app/static/js/1.cd822512.chunk.js"
+  },
+  {
+    "revision": "5b39ef2b605ad071cb88",
+    "url": "/productivity-app/static/css/main.77a7f951.chunk.css"
+  },
+  {
+    "revision": "6992c63f90ca531fac70f70f4afe507e",
+    "url": "/productivity-app/index.html"
+  }
+];
